0941ad76a373a6944cc57d6e98d94ccc8935a61b
Yefry Calderón <yefry.calderon@udea.edu.co>
2016-01-17 14:20:57 -0500

    adding cross origin CORS

--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -4,43 +4,43 @@
 
 angular.module('ubicameUdeaApp.services', [])
 	.factory('Bloque', function ($resource) {
-    //return $resource('http://ubicame-udea.herokuapp.com/bloques/:id', { id : '@_id' }, {
-	return $resource('http://localhost:3000/bloques/:id', { id : '@_id' }, {
+    return $resource('http://ubicame-udea.herokuapp.com/bloques/:id', { id : '@_id' }, {
+	//return $resource('http://localhost:3000/bloques/:id', { id : '@_id' }, {
         update: {
             method: 'PUT'
         }
     });})
 	.factory('TipoUnidad', function ($resource){
-	//return $resource('http://ubicame-udea.herokuapp.com/bloques/:id', { id : '@_id' }, {
-	return $resource('http://localhost:3000/tiposunidad/:id', { id : '@_id' }, {
+	return $resource('http://ubicame-udea.herokuapp.com/tiposunidad/:id', { id : '@_id' }, {
+	//return $resource('http://localhost:3000/tiposunidad/:id', { id : '@_id' }, {
         update: {
             method: 'PUT'
         }
     });})
 	.factory('Unidad', function ($resource){
-	//return $resource('http://ubicame-udea.herokuapp.com/bloques/:id', { id : '@_id' }, {
-	return $resource('http://localhost:3000/unidades/:id', { id : '@_id' }, {
+	return $resource('http://ubicame-udea.herokuapp.com/unidades/:id', { id : '@_id' }, {
+	//return $resource('http://localhost:3000/unidades/:id', { id : '@_id' }, {
         update: {
             method: 'PUT'
         }
     });})
 	.factory('Departamento', function ($resource){
-	//return $resource('http://ubicame-udea.herokuapp.com/bloques/:id', { id : '@_id' }, {
-	return $resource('http://localhost:3000/departamentos/:id', { id : '@_id' }, {
+	return $resource('http://ubicame-udea.herokuapp.com/departamentos/:id', { id : '@_id' }, {
+	//return $resource('http://localhost:3000/departamentos/:id', { id : '@_id' }, {
         update: {
             method: 'PUT'
         }
     });})
 	.factory('Ubicacion', function ($resource){
-	//return $resource('http://ubicame-udea.herokuapp.com/bloques/:id', { id : '@_id' }, {
-	return $resource('http://localhost:3000/ubicaciones/:id', { id : '@_id' }, {
+	return $resource('http://ubicame-udea.herokuapp.com/ubicaciones/:id', { id : '@_id' }, {
+	//return $resource('http://localhost:3000/ubicaciones/:id', { id : '@_id' }, {
         update: {
             method: 'PUT'
         }
     });})
 	.factory('Usuario', function ($resource){
-	//return $resource('http://ubicame-udea.herokuapp.com/bloques/:id', { id : '@_id' }, {
-	return $resource('http://localhost:3000/usuarios/:id', { id : '@_id' }, {
+	return $resource('http://ubicame-udea.herokuapp.com/usuarios/:id', { id : '@_id' }, {
+	//return $resource('http://localhost:3000/usuarios/:id', { id : '@_id' }, {
         update: {
             method: 'PUT'
         }
